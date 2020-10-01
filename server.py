2c0ac3613a0ed2d628c2200efccdf1fffa7ad182
gdmen <gmenezes@alumni.upenn.edu>
2020-09-30 20:30:08 -0700

    Add a real last modified date.

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import datetime
 import flask
 import glob
 import os
@@ -9,23 +10,30 @@ app = flask.Flask(
     static_url_path=''
 )
 
-markdown_paths = glob.glob('%s/*.md' % MARKDOWN_DIR)
-markdown_files = [os.path.splitext(os.path.basename(path))[0] for path in markdown_paths]
+class FileInfo:
+
+    def __init__(self, name, last_modified):
+        self.name = name
+        self.last_modified = datetime.datetime.fromtimestamp(last_modified).strftime('%b %d, %Y')
+
+md_paths = glob.glob('%s/*.md' % MARKDOWN_DIR)
+md_file_infos = [FileInfo(os.path.splitext(os.path.basename(path))[0], os.path.getmtime(path)) for path in md_paths]
+md_names = [fi.name for fi in md_file_infos]
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html', markdown_files=markdown_files)
+    return flask.render_template('index.html', file_infos=md_file_infos)
 
 @app.route('/<path>')
 @app.route('/<path:path>')
 def show(path):
     path = str(path)
-    if path in markdown_files:
+    if path in md_names:
         with open('%s/%s.md' % (MARKDOWN_DIR, path), 'r') as f:
-            markdown = f.read()
-        markdown = markdown.replace('"', '\\"')
-        markdown = markdown.replace('\n', '\\n')
-        return flask.render_template('print.html', markdown_files=markdown_files, markdown=markdown)
+            md = f.read()
+        md = md.replace('"', '\\"')
+        md = md.replace('\n', '\\n')
+        return flask.render_template('print.html', markdown=md)
     else:
         return app.send_static_file(path)
 
