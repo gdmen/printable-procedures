f53953a466816904ca0641a38eafce1abd5d78c1
gdmen <gmenezes@alumni.upenn.edu>
2020-10-01 19:47:19 -0700

    Add live-checking of the procedures directory.

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import flask
 import glob
 import os
 
-MARKDOWN_DIR = 'markdown'
+MARKDOWN_DIR = 'procedures'
 
 app = flask.Flask(
     __name__,
@@ -16,17 +16,19 @@ class FileInfo:
         self.name = name
         self.last_modified = datetime.datetime.fromtimestamp(last_modified).strftime('%b %d, %Y')
 
-md_paths = glob.glob('%s/*.md' % MARKDOWN_DIR)
-md_file_infos = [FileInfo(os.path.splitext(os.path.basename(path))[0], os.path.getmtime(path)) for path in md_paths]
-md_names = [fi.name for fi in md_file_infos]
+def GetFileInfos():
+    md_paths = glob.glob('%s/*.md' % MARKDOWN_DIR)
+    md_file_infos = [FileInfo(os.path.splitext(os.path.basename(path))[0], os.path.getmtime(path)) for path in md_paths]
+    return md_file_infos
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html', file_infos=md_file_infos)
+    return flask.render_template('index.html', file_infos=GetFileInfos())
 
 @app.route('/<path>')
 @app.route('/<path:path>')
 def show(path):
+    md_names = [fi.name for fi in GetFileInfos()]
     path = str(path)
     if path in md_names:
         with open('%s/%s.md' % (MARKDOWN_DIR, path), 'r') as f:
